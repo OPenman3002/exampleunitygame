df1ca4b4ae2f7815f97df5a7ccffd5c7594eaa82
OWEN PENMAN <1736953@student.fife.ac.uk>
2018-03-07 10:10:51 +0000

    Basic physics and collision
    Added seperated graphics and added gravity/physics to mario

---
+++ Assets/Script/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	string toPrintClass = "Smokin doinks bigger than king kong's fingers";
+
+	// Use this for initialization
+	void Start () {
+		string toPrint = "Mah name ah jeff HAHAHAHAHAHAHAHAHAHAHAH";
+		Debug.Log (toPrintClass);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
