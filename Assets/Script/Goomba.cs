8c28f18bbf10a34ed2d121a78a483546fe5f716a
OWEN PENMAN <1736953@student.fife.ac.uk>
2018-03-14 10:42:54 +0000

    addedgoombadamage

---
+++ Assets/Script/Goomba.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : MonoBehaviour {
+
+	// Public variables (visible in editor)
+	public float damage = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Called when trigger collides with another collider
+	void OnTriggerStay2D(Collider2D other)
+
+	{
+        // attempting to get the player script from what Goomba collides with. 
+        player playerscript = other.GetComponent<player>();
+        // if player script exists
+        if (playerscript != null) {
+            playerscript.Damage(damage);
+            Debug.Log("Goomba dealt damage to player =" + damage);
+        }
+
+
+	} // end onTriggerEnter2D	
+}
+
+
--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	string toPrintClass = "Smokin doinks bigger than king kong's fingers";
+
+	public float speed = 5;
+	public float health = 100;
+    public float invulDuration = 1;
+
+    private float invulEndTime = 0;
 
 	// Use this for initialization
-	void Start () {
-		string toPrint = "Mah name ah jeff HAHAHAHAHAHAHAHAHAHAHAH";
-		Debug.Log (toPrintClass);
-		
+	void start() {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
+
+		ourRigidBody.velocity = Vector2.right * speed;
+	} 
+	public void Damage(float damageToDeal)
+	{
+        if (Time.time >= invulEndTime) {
+            // reducting health
+            health = health - damageToDeal;
+
+            //TODO: Handle Death
+
+            //Set us as invurnerable for a set duration
+            invulEndTime = Time.time + invulDuration;
+
+            // Log the result of the function
+            Debug.Log("Damage was dealth");
+            Debug.Log("damageTodeal =" + damageToDeal);
+            Debug.Log("health = " + health);
+        }
+        
+	} // end damage 
 }
